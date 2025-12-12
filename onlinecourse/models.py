70cc60d3f70c62a579557ac04d5d0337368b85ec
Jorjah <jorjah.sloan@gmail.com>
2025-12-12 09:19:45 -0500

    initial commit, Question model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -67,6 +67,23 @@ class Course(models.Model):
         return "Name: " + self.name + "," + \
                "Description: " + self.description
 
+#Question model
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    content = models.CharField(max_length=200)
+    grade = models.IntegerField(default=50)
+
+    def __str__(self):
+        return "Question: " + self.content
+
+    #method to calculate if the learner gets the score of the question
+    def is_get_score(self, selected_ids):
+        all_answers = self.choice_set.filter(is_correct=True).count()
+        selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+        if all_answers == selected_correct:
+            return True
+        else:
+            return False
 
 # Lesson model
 class Lesson(models.Model):
