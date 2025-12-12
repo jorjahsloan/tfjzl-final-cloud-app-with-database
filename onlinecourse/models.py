f3286dd5e643aa667739b6d3eabab85c139f0e9d
Jorjah <jorjah.sloan@gmail.com>
2025-12-12 09:29:19 -0500

    choice model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -85,6 +85,13 @@ class Question(models.Model):
         else:
             return False
 
+# Choice model
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=200)
+    is_correct = models.BooleanField(default=False)
+
+
 # Lesson model
 class Lesson(models.Model):
     title = models.CharField(max_length=200, default="title")
